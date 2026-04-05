3e8849c2793e352349c2b01dd08a9ffeb4f6590e
Matheus <matheusdavi31@gmail.com>
2026-04-05 19:09:02 -0300

    Fix: Inicialização do database on API Startup

--- src/api.py
+++ src/api.py
@@ -1,17 +1,41 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 import pandas as pd
+import os
+
+from src.scraper import get_jobs
 
 app = FastAPI()
 
-engine = create_engine("sqlite:///data/jobs.db")
+DB_PATH = "data/jobs.db"
+engine = create_engine(f"sqlite:///{DB_PATH}")
+
+
+def initialize_database():
+    os.makedirs("data", exist_ok=True)
+
+    if not os.path.exists(DB_PATH):
+        vacancies = get_jobs()
+
+        df = pd.DataFrame(vacancies)
+
+        df = df[df["title"].str.contains("Python", case=False)]
+
+        df.to_sql("jobs", engine, if_exists="replace", index=False)
+
+
+@app.on_event("startup")
+def startup_event():
+    initialize_database()
+
 
 @app.get("/")
 def home():
     return {"mensagem": "API de vagas rodando 🚀"}
 
+
 @app.get("/jobs")
-def get_jobs(title: str = None):
+def get_all_jobs(title: str = None):
     query = "SELECT * FROM jobs"
 
     if title:
